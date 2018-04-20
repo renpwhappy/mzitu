8490e0ee1b849dc7b94267f0c6fe71b874cec6f8
peiwei.ren <peiwei.ren@yingzt.com>
2018-04-20 22:01:49 +0800

    set file encoding and json data in analysis encoding

--- analysis.py
+++ analysis.py
@@ -1,15 +1,15 @@
+# -*- coding: UTF-8 -*-
 import os
 import json
 import jieba
 import matplotlib.pyplot as plt
 from wordcloud import WordCloud
 
-
 # 将所有文件夹名转换为str类型
-folder_name = " ".join(os.listdir(r"E:\Pic\mzitu"))
+folder_name = " ".join(os.listdir(r"/Users/renpeiwei/Pictures/mzitu"))
 
 # jieba分词
-jieba.load_userdict(r".\data\jieba.txt")
+jieba.load_userdict(r"./data/jieba.txt")
 seg_list = jieba.lcut(folder_name, cut_all=False)
 
 # 利用字典统计词频
@@ -22,12 +22,12 @@ counter_sort = sorted(counter.items(), key=lambda value: value[1], reverse=True)
 # print(counter_sort)
 
 # 解析成 json 类型并写入文件
-words = json.dumps(counter_sort, ensure_ascii=False)
-with open(r".\data\words.json", "w+", encoding="utf-8") as f:
+words = json.dumps(counter_sort, ensure_ascii=False).encode("utf-8")
+with open(r"./data/words.json", "w+") as f:
     f.write(words)
 
 # 生成词云
-wordcloud = WordCloud(font_path=r".\font\msyh.ttf",
+wordcloud = WordCloud(font_path=r"./font/msyh.ttf",
                       max_words=100, height=600, width=1200).generate_from_frequencies(counter)
 plt.imshow(wordcloud)
 plt.axis('off')
